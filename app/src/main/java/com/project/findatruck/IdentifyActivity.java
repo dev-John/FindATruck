d268757205f434c1fbdfed12f6c0ff83bcf55b13
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2019-12-01 20:21:43 -0300

    first commit

---
+++ app/src/main/java/com/project/findatruck/IdentifyActivity.java
@@ -0,0 +1,14 @@
+package com.project.findatruck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdentifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identify);
+    }
+}
