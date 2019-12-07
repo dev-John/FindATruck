bc383662756d9982a07b719ce125bba7ab72a161
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2019-12-07 10:23:48 -0300

    Ajuste no fechamento das activities

--- app/src/main/java/com/project/findatruck/InfoActivity.java
+++ app/src/main/java/com/project/findatruck/InfoActivity.java
@@ -18,5 +18,6 @@ public class InfoActivity extends AppCompatActivity {
         Intent intent = new Intent(InfoActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(intent);
+        finish();
     }
 }
